12f57f160612f7ff1092f1c31b0c781f0d86fd69
saulGoodz <saulgooding@dunsterhouse.co.uk>
2022-05-04 14:09:40 +0100

    complete re-useable instagram feed component - using facebook's access token

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,11 @@ import InstagramGallery from "./components/InstagramGallery.vue";
 </script>
 
 <template>
-  <InstagramGallery :count="24" accessToken="" />
+  <InstagramGallery
+    :count="5"
+    accessToken="IGQVJXREN4ZA1VZARXItZAGphaXpwb2tObWk4S1NQbTlCczMzWHV4X2xYVjYtTHM5VngwNjdUZAF9ORjFLTmc0SDdhRTZARTGVWR3ZAvZAHAwVWQweVJSczE0M3ZAzRVM0T210NzVOb0NVMU1Ua3MwN0pJZA3U5VwZDZD"
+    :pagination="true"
+  />
 </template>
 
 <style>
--- src/components/InstagramGallery.vue
+++ src/components/InstagramGallery.vue
@@ -15,8 +15,7 @@ const usePagination = ref(false);
 const paginationNextUrl = ref("");
 const paginationPrevUrl = ref("");
 
-onMounted(() => {
-  const url = `https://graph.instagram.com/me/media?fields=media_count,media_type,permalink,media_url,caption&limit=${props.count}&access_token=${props.accessToken}`;
+const fetchInstaData = (url) => {
   axios
     .get(url)
     .then((response) => {
@@ -26,8 +25,16 @@ onMounted(() => {
         hasError.value = true;
       } else {
         instagramData.value = response.data;
-        console.log(instagramData._rawValue);
+        if (instagramData !== null) {
+          paginationNextUrl.value = instagramData._rawValue.paging.next;
+          paginationPrevUrl.value = instagramData._rawValue.paging.previous;
+        }
         isLoading.value = false;
+      }
+    })
+    .then(() => {
+      if (props.pagination) {
+        usePagination.value = props.pagination;
       }
     })
     .catch((error) => {
@@ -35,16 +42,92 @@ onMounted(() => {
       hasError.value = true;
       isLoading.value = false;
     });
+};
+
+onMounted(() => {
+  const url = `https://graph.instagram.com/me/media?fields=media_count,media_type,permalink,media_url,caption&limit=${props.count}&access_token=${props.accessToken}`;
+  fetchInstaData(url);
 });
+
+const handlePaginationNext = () => {
+  fetchInstaData(paginationNextUrl.value);
+};
+
+const handlePaginationPrev = () => {
+  fetchInstaData(paginationPrevUrl.value);
+};
 </script>
 
 <template>
-  <h1>INSTAGRAM GALLERY</h1>
-  <div>
-    <h1 v-if="isLoading != false">LOADING...</h1>
-    <h1 v-if="hasError != false">Ooops, something went wrong.</h1>
-    <h1 v-else>{{ instagramData }}</h1>
+  <h1>VUE3 INSTAGRAM GALLERY</h1>
+  <div class="container">
+    <h1 v-if="isLoading">LOADING...</h1>
+    <h1 v-else="hasError">Ooops, something went wrong.</h1>
+    <div v-else class="gallery">
+      <div
+        v-for="image in instagramData.data"
+        :key="image.id"
+        class="gallery-item"
+      >
+        <a
+          :href="image.permalink"
+          :key="image.id"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img
+            v-if="
+              image.media_type === 'IMAGE' ||
+              image.media_type === 'CAROUSEL_ALBUM'
+            "
+            :src="image.media_url"
+            :alt="image.caption"
+            :key="image.id"
+            class="gallery-image"
+          />
+          <video v-else :key="image.id" class="gallery-image">
+            <source :src="image.media_url" type="video/mp4" />
+          </video>
+        </a>
+      </div>
+    </div>
+    <div v-if="usePagination">
+      <div v-if="paginationPrevUrl">
+        <button class="" type="button" @click="handlePaginationPrev">
+          Previous
+        </button>
+      </div>
+      <div v-if="paginationNextUrl">
+        <button class="" type="button" @click="handlePaginationNext">
+          Next
+        </button>
+      </div>
+    </div>
   </div>
 </template>
 
-<style></style>
+<style>
+.container {
+  max-width: 93.5rem;
+  margin: 0 auto;
+  padding: 0 2rem;
+}
+.gallery {
+  display: flex;
+  flex-wrap: wrap;
+  margin: -1rem, -1rem;
+  padding-bottom: 3rem;
+}
+.gallery-item {
+  position: relative;
+  flex: 1 0 22rem;
+  margin: 1rem;
+  color: #fff;
+  cursor: pointer;
+}
+.gallery-image {
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+}
+</style>
